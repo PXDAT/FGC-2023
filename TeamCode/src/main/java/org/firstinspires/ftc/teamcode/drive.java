10adefc80f6b8f9d6a9c2817440bc0389e9463d3
Phạm Xuân Đạt <136553197+PXDAT@users.noreply.github.com>
2023-08-18 09:46:37 +0700

    drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -15,58 +14,53 @@ import org.firstinspires.ftc.robotcontroller.external.samples.ConceptGamepadRumb
 
 @TeleOp(name = "Main")
 public class drive extends LinearOpMode {
-
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
-    public boolean state = false, state1= false;
+    public boolean state = false, state1 = false;
     private Servo hook1 = null;
     private Servo hook2 = null;
     private Servo door = null;
     private CRServo storage_lift = null;
-    private DcMotor climber = null, intake = null, bot_lift = null;
-    
+    private DcMotor climber = null, intake = null;
+    private Servo bot_lift = null;
+
     DcMotorEx shooter;
     double integralSum = 0;
-    double Kp = 6.8, Ki = 7.7, Kd = 10, Kf = 10;
+    double Kp = 200, Ki = 5, Kd = 2000, Kf = 10;
     int count = 0;
-    Gamepad currentGamepad1 = new Gamepad();
-    Gamepad currentGamepad2 = new Gamepad();
-
-    Gamepad previousGamepad1 = new Gamepad();
-    Gamepad previousGamepad2 = new Gamepad();
 
     ElapsedTime timer = new ElapsedTime();
     private double lastError = 0;
+
     @Override
     public void runOpMode() {
 //        core hex
         climber = hardwareMap.get(DcMotor.class, "climber");
-        bot_lift = hardwareMap.get(DcMotor.class,"bot_lift");
 //        HD motor
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "lf");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "lb");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "lf");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "lb");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rf");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rb");
         shooter = hardwareMap.get(DcMotorEx.class, "shooter");
         intake = hardwareMap.get(DcMotor.class, "intake");
 //        servo setup
-        hook1 = hardwareMap.get(Servo.class,"hook1");
+        hook1 = hardwareMap.get(Servo.class, "hook1");
         hook2 = hardwareMap.get(Servo.class, "hook2");
         door = hardwareMap.get(Servo.class, "door");
         storage_lift = hardwareMap.get(CRServo.class, "storage_lift");
+        bot_lift = hardwareMap.get(Servo.class, "bot_lift");
 //        Motor driver set direction
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        intake.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.FORWARD);
 //        PID setup
-        shooter.setDirection(DcMotor.Direction.REVERSE);
-
+        shooter.setDirection(DcMotor.Direction.FORWARD);
         shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -78,96 +72,62 @@ public class drive extends LinearOpMode {
         while (opModeIsActive()) {
             // tank drive with joystick
             leftFrontDrive.setPower(gamepad1.left_stick_y);
-            rightFrontDrive.setPower(-gamepad1.right_stick_y);
-            leftBackDrive.setPower(gamepad1.left_stick_y);
+            rightFrontDrive.setPower(gamepad1.right_stick_y);
+            leftBackDrive.setPower(-gamepad1.left_stick_y);
             rightBackDrive.setPower(-gamepad1.right_stick_y);
-            if(gamepad1.left_trigger == 0 & gamepad1.right_trigger == 0)
-            {
+            if (gamepad1.left_trigger == 0 & gamepad1.right_trigger == 0) {
                 hook1.setPosition(0.5);
                 hook2.setPosition(0.5);
-            }
-            else if(gamepad1.left_trigger > 0)
-            {
-                hook1.setPosition(hook1.getPosition()+0.05);
-                hook2.setPosition(hook2.getPosition()+0.05);
                 leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-                leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-                rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-                rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-
-            }
-            else if(gamepad1.right_trigger > 0)
-            {
-                hook1.setPosition(hook1.getPosition()-0.05);
-                hook2.setPosition(hook2.getPosition()-0.05);
-                leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
                 leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
                 rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+            } else if (gamepad1.left_trigger > 0) {
+                hook1.setPosition(1.0);
+                hook2.setPosition(hook2.getPosition() + 0.05);
+                leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+                leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+            } else if (gamepad1.right_trigger > 0) {
+                hook1.setPosition(-1.0);
+                hook2.setPosition(hook2.getPosition() - 0.05);
+                leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+                leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
                 rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
             }
-
-            if(gamepad1.left_bumper)
-            {
-                count++;
-            }
-            switch(count % 2)
-            {
-                case(1):
-                    double shooterPower = PIDControl(1000, shooter.getCurrentPosition());
-                    shooter.setVelocity(2000);
-                    break;
-                case(0):
-                    shooter.setVelocity(0);
-                    break;
+            if (gamepad1.left_bumper) {
+                intake.setPower(1);
             }
             if(gamepad1.right_bumper)
             {
-                state1 =! state1;
+                intake.setPower(0);
             }
-            if(state1)
-            {
-                intake.setPower(1);
+            if (gamepad2.left_trigger > 0) {
+                double shooterPower = PIDControl(1000, shooter.getCurrentPosition());
+                shooter.setVelocity(2000);
             }
             else
             {
-                intake.setPower(0);
+                shooter.setVelocity(0);
             }
-            if(gamepad1.dpad_up)
+            if (gamepad2.right_bumper)
             {
                 climber.setPower(1);
-            }
-            else if(gamepad1.dpad_down)
-            {
-                climber.setPower(-1);
             }
             else
             {
                 climber.setPower(0);
             }
-            if(gamepad1.square)
-            {
-                bot_lift.setPower(1);
+            if (gamepad2.triangle) {
+                door.setPosition(door.getPosition() + 0.1);
             }
-            else if(gamepad1.circle)
-            {
-                bot_lift.setPower(-1);
-            }
-            else
-            {
-                bot_lift.setPower(0);
-            }
-            if(gamepad1.dpad_left)
-            {
-                door.setPosition(door.getPosition()+0.1);
-            }
-            if(gamepad1.dpad_right)
-            {
-                door.setPosition(door.getPosition()-0.1);
-            }
-            else
-            {
+            else if (gamepad2.cross) {
                 door.setPosition(0.5);
             }
+            storage_lift.setPower(gamepad2.left_stick_y);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -175,18 +135,19 @@ public class drive extends LinearOpMode {
         }
     }
 //    PID CONTROL
-    public double PIDControl(double reference, double state){
-        double error = reference - state;
+        public double PIDControl(double reference, double state)
+        {
+            double error = reference - state;
 
-        integralSum += error + timer.seconds();
-        double derivative = (error - lastError) / timer.seconds();
+            integralSum += error + timer.seconds();
+            double derivative = (error - lastError) / timer.seconds();
 
-        lastError = error;
+            lastError = error;
 
 
-        timer.reset();
+            timer.reset();
 
-        double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki)+ (reference * Kf);
-        return output;
-    }
+            double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki) + (reference * Kf);
+            return output;
+        }
 }
